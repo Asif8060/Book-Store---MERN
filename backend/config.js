29b2795bcdd518971d9c8749fefd5e098756cf31
Asif8060 <asifansari55000@gmail.com>
2024-08-15 15:59:29 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://asif1033:asif1349@books-store-mern.u2nno.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
